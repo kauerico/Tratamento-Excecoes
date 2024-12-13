da5f4cb82b244048b6f503d27d93549b3ca6583c
Kaue <ribeirokaue70@gmail.com>
2024-12-12 23:41:05 -0400

    Adicionando as entradas do usuário

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+
+        // Entrada do usuario.
+        int numero; 
+        String nome; 
+        double saldo, limite, valorsaque;
+
+        System.out.println("Informe os dados da conta: ");
+        System.out.println("Numero: ");
+        numero = teclado.nextInt();
+        System.out.println("Saldo inicial: ");
+        saldo = teclado.nextDouble();
+        System.out.println("Limite de saque: ");
+        limite = teclado.nextDouble();
+        System.out.println("Informe o valor para saque: ");
+        valorsaque = teclado.nextDouble();
+        System.out.println("Novo saldo: ");
+        System.out.println("Informe o valor para depósito: ");
+        System.out.println("Novo saldo: ");
+
     }
 }
